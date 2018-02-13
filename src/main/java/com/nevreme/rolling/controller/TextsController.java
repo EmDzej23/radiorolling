a733073f075f900aabf7647303b9f314d0fe5ef5
markoj <marko.jereminov@plusplusnt.rs>
2018-02-13 10:42:51 +0100

    Master:Pocetna strana

---
+++ src/main/java/com/nevreme/rolling/controller/TextsController.java
@@ -0,0 +1,54 @@
+package com.nevreme.rolling.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.nevreme.rolling.service.PlaylistService;
+import com.nevreme.rolling.service.VideoService;
+import com.nevreme.rolling.utils.PlaylistUtils;
+
+@Controller
+@RequestMapping("/text")
+public class TextsController {
+	
+	@Autowired
+	VideoService videoService;
+	
+	@Autowired
+	PlaylistService playlistService;
+	
+	@Autowired
+	PlaylistUtils playlistUtils;
+	
+	private boolean started=false;
+	
+	@RequestMapping(value = { "/","" }, method = RequestMethod.GET)
+	public ModelAndView addPost() {
+		ModelAndView modelAndView = new ModelAndView();
+//		Long playlist_id = playlistService.getPlaylistByName("Inserti Iz Filmova");
+//		modelAndView.addObject("playlist_id",playlist_id);
+//		modelAndView.addObject("appRoot",System.getProperty("APP_ROOT"));
+//		modelAndView.addObject("ws",System.getProperty("WS")==null?"":System.getProperty("WS"));
+		modelAndView.setViewName("admin/text");
+		return modelAndView;
+	}
+	
+//	@RequestMapping(value = { "{name}/","{name}" }, method = RequestMethod.GET)
+//	public ModelAndView ids(@PathVariable String name) {
+//		ModelAndView modelAndView = new ModelAndView();
+//		Long playlist_id = playlistService.getPlaylistByName(name);
+//		modelAndView.addObject("playlist_id",playlist_id);
+//		modelAndView.addObject("appRoot",System.getProperty("APP_ROOT"));
+//		modelAndView.addObject("ws",System.getProperty("WS")==null?"":System.getProperty("WS"));
+//		modelAndView.setViewName("admin/video");
+//		return modelAndView;
+//	}
+	
+	
+	
+	
+}
