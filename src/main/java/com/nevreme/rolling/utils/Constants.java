85a63f4e53997260f93eb7425d239d81a30dbb0c
jelenas <jelena.sijan@plusplusnt.rs>
2018-05-22 11:00:25 +0200

    Revert: Cookie path

--- src/main/java/com/nevreme/rolling/utils/Constants.java
+++ src/main/java/com/nevreme/rolling/utils/Constants.java
@@ -3,8 +3,7 @@ package com.nevreme.rolling.utils;
 import java.io.File;
 
 public class Constants {
-//	public final static String APP_ROOT = "http://radiorolling.com";
-	public final static String APP_ROOT = "http://localhost:8081";
+	public final static String APP_ROOT = "http://radiorolling.com";
 	public final static String IMAGE_PATH = File.separator + "var" + File.separator + "lib" + File.separator + "rolling"
 			+ File.separator + "res" + File.separator + "images";
 	public final static String IMAGES_CONFIG_LOCATION = "file:///var/lib/rolling/res/images/";
